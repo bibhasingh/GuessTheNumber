db8f5dea1349a6e6cb8c71a54a75aea056da04f7
Bibha Singh <bibhasingh@Bibhas-MacBook-Air.local>
2023-07-23 10:31:54 +0200

    my first javaScript game

---
+++ script.js
@@ -0,0 +1,47 @@
+'use strict';
+
+let secNum = Math.trunc(Math.random() * 20) + 1;
+
+let score = Number(document.querySelector('.score').textContent);
+let highScore = 0;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const userGuess = document.querySelector('.guess').value;
+  console.log(userGuess, secNum);
+
+  if (score > 0) {
+    if (userGuess > secNum) {
+      document.querySelector('.message').textContent = '📈 Too high ';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else if (secNum > userGuess) {
+      document.querySelector('.message').textContent = '📉 Too low ';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🎉 Correct guess ';
+
+      document.querySelector('.number').textContent = secNum;
+      document.querySelector('body').style.backgroundColor = '#60b347';
+      document.querySelector('.number').style.width = '30rem';
+
+      if (score > highScore) {
+        highScore = score;
+        document.querySelector('.highscore').textContent = highScore;
+      }
+    }
+  } else {
+    document.querySelector('.message').textContent = '😭 You lost the game ';
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('.message').textContent = 'Start guessing... ';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  secNum = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+});
